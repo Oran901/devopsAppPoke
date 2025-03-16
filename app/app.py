77bba0557b8e8eacb919653c3f8cf58a93cb4197
Oran901 <oranxbox@gmail.com>
2025-03-16 12:26:07 +0200

    added delete feature

--- app/app.py
+++ app/app.py
@@ -13,7 +13,8 @@ def index():
     response = requests.get(f"{API_URL}")
     if response.status_code == 200:
         quotes_data = response.json()
-        quotes = [{"text": q.get("quote", ""), "author": q.get("author", "")} for q in quotes_data]
+        quotes = [{"id": q.get("id"), "text": q.get("quote", ""), "author": q.get("author", "")} for q in quotes_data]
+        # quotes = [{"text": q.get("quote", ""), "author": q.get("author", "")} for q in quotes_data]
     else:
         quotes = []
     return render_template("index.html", quotes=quotes)
@@ -24,6 +25,12 @@ def submit():
     author = request.form["author"]
     requests.post(f"{API_URL}", json={"quote": quote, "author": author})
     return redirect(url_for("index"))
+
+@app.route("/delete/<quote_id>", methods=["POST"])
+def delete(quote_id):
+    response = requests.delete(f"{API_URL}/{quote_id}")
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
